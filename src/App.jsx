dc2e89a860c70e6c9aca4d6106a0f571800bbb6a
esteban fernandez <estebantofi@gmail.com>
2025-10-06 21:43:28 -0300

    add nav and footer, install react-router-dom.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
+import { Routes } from "react-router-dom";
+import "./App.css";
+
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
-
-
   return (
     <>
-     
+      <NavBar />
+      <Routes></Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
